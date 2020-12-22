922448c4f54eab3add26a32748b1485cd4102d2f
Sonawap <paulsola79@gmail.com>
2020-12-22 12:02:13 -0800

    k

--- core/config/config.php
+++ core/config/config.php
@@ -21,14 +21,14 @@ Go to setting -> developer to see your keys*/
 // SET SECRET KEY HERE
 define(
 	'SECRET', 
-	'sk_test_64ad293134c658b279a1ecdf7138ceedfd8a9aa6' ///replace with your secret key
+	'sk_test_' ///replace with your secret key
 );
 
 // SET PUBLIC KEY HERE
 
 define(
 	'PUBLIC',
-	'pk_test_52e190d965738d882357fae33760b8e4150a00ae' ///replace with your public key
+	'pk_test_' ///replace with your public key
 );
 
 
